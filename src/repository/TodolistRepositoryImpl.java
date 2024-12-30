00f729a59dae973f112fc9a7d56fb9c57c36d6c7
ELghazX <2309106041@student.unmul.ac.id>
2024-12-31 06:26:08 +0800

    feat(repository): implement add and resize array

--- src/repository/TodolistRepositoryImpl.java
+++ src/repository/TodolistRepositoryImpl.java
@@ -1,7 +1,6 @@
 package repository;
 
 import entity.Todolist;
-import repository.TodolistRepository;
 
 public class TodolistRepositoryImpl implements TodolistRepository {
   public Todolist[] data = new Todolist[10];
@@ -10,7 +9,37 @@ public class TodolistRepositoryImpl implements TodolistRepository {
     return data;
   }
 
+  public boolean isFull() {
+    var isFull = true;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] == null) {
+        isFull = false;
+      }
+      break;
+    }
+    return isFull;
+  }
+
+  public void resizeIfFull() {
+    if (isFull()) {
+      var temp = data;
+      data = new Todolist[data.length * 2];
+
+      for (var i = 0; i < temp.length; i++) {
+        data[i] = temp[i];
+      }
+    }
+  }
+
   public void add(Todolist todolist) {
+    resizeIfFull();
+    for (var i = 0; i < data.length; i++) {
+
+      if (data[i] == null) {
+        data[i] = todolist;
+        break;
+      }
+    }
 
   }
 
